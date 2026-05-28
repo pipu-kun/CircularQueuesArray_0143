09e907f6ec228dfc7c0e51cb5ce01426a0967cba
pipu-kun <SideActivity@proton.me>
2026-05-28 10:18:49 +0700

    Add private member

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class Queues{};
+class Queues{
+    private:
+        static const int max = 3;
+        int FRONT, REAR;
+        int queue_array[max];
+};
