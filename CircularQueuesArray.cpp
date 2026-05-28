a0decf712da7ae32bd37b286c9447ce5120eb20c
pipu-kun <SideActivity@proton.me>
2026-05-28 10:43:35 +0700

    else condition

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -45,6 +45,11 @@ class Queues{
             if (FRONT == REAR){
                 FRONT =- 1;
                 REAR =- 1;
+            } else {
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    FRONT = FRONT + 1;
             }
         }
 };
