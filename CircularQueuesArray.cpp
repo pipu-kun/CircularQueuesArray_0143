6f04fa167e0e4ac9a0bba69155c5ca0e3c1d9bdf
pipu-kun <SideActivity@proton.me>
2026-05-28 10:35:50 +0700

    Circular logic

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -26,6 +26,12 @@ class Queues{
             if (FRONT ==- 1){ // while empty
                 FRONT = 0;
                 REAR = 0;
+            } else { // array(last index) then = index 0
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
             }
+            queue_array [REAR] = num;
         }
 };
