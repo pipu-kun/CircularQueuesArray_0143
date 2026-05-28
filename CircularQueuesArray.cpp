cc78a2217540b205b1395711283e2feb9f1e3c39
pipu-kun <SideActivity@proton.me>
2026-05-28 10:31:50 +0700

    Empty Queue Condition

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -18,9 +18,14 @@ class Queues{
             cin >> num;
             cout << endl;
 
-            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)){
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)){ 
                 cout << "\nQueue overflow\n";
                 return;
+            }
+
+            if (FRONT ==- 1){ // while empty
+                FRONT = 0;
+                REAR = 0;
             }
         }
 };
